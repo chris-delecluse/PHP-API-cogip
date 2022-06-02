e591256d7c1a587fc2c545c7688c40c6d3cd271d
Christopher Delecluse <christopherdeleclusepro@gmail.com>
2022-06-02 17:41:42 +0200

    add notFound style

--- views/notFound.php
+++ views/notFound.php
@@ -1,7 +1,19 @@
 <?php $title = "404 not found" ?>
 <?php ob_start() ?>
 
-    <h1 class="mt-10 text-3xl font-bold text-center">404 not found !</h1>
+    <main class="h-screen w-full flex flex-col justify-center items-center bg-[#1A2238]">
+        <h1 class="text-9xl font-extrabold text-white tracking-widest">404</h1>
+        <div class="bg-[#FF6A3D] px-2 text-sm rounded rotate-12 absolute">Page Not Foundx</div>
+        <button class="mt-5">
+            <a href="/"
+               class="relative inline-block text-sm font-medium text-[#FF6A3D] group active:text-orange-500 focus:outline-none focus:ring">
+                <span class="absolute inset-0 transition-transform translate-x-0.5 translate-y-0.5 bg-[#FF6A3D] group-hover:translate-y-0 group-hover:translate-x-0"></span>
+                <span class="relative block px-8 py-3 bg-[#1A2238] border border-current">
+          <router-link to="/">Go Home</router-link>
+                </span>
+            </a>
+        </button>
+    </main>
 
 <?php $content = ob_get_clean() ?>
 <?php require('layouts/layout.php') ?>
